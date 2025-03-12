19896bbbb1c14a25f1c44146551888b358c0ec96
Denis <larrrrrryanderson@gmail.com>
2025-03-12 18:50:50 +0300

    Added screen

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
+from kivy.lang import Builder
 from kivymd.app import MDApp
+
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.screenmanager import MDScreenManager
+
+class FridgeScreen(MDScreen):
+    pass
+
+class DishesScreen(MDScreen):
+    pass
+
+class CartScreen(MDScreen):
+    pass
+
+class ProfileScreen(MDScreen):
+    pass
 
 class KitchenApp(MDApp):
     def build(self):
-        return
+        # Set themese and palet
+        self.theme_cls.theme_style = 'Dark'
+        self.theme_cls.primary_palet = 'Green'
+        # Create screen_manager
+        self.screen_manager = MDScreenManager()
+        self.screen_manager.add_widget(FridgeScreen(name = 'fridge'))
+        self.screen_manager.add_widget(ProfileScreen(name = 'profile'))
+        
+        return self.screen_manager
 
 if __name__ == '__main__':
     KitchenApp().run()
